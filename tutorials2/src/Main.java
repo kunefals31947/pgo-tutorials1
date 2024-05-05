ab19eb894fb83a5738088b734a9ec6c6de47b452
Your GitHub Username <Your GitHub Email>
2024-05-05 10:40:59 +0200

    C:\Users\Krzysiek\pgo-tutorials1\tut3\src

---
+++ tutorials2/src/Main.java
@@ -0,0 +1,25 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static void main(String[] args) {
+    student st=new student();
+    st.name="john";
+    st.surname="johnsky";
+    st.index="0001";
+
+    student_group stg=new student_group();
+    stg.nazwa="grupa1";
+    stg.wielkoscgrupy="10";
+
+    
+
+
+
+
+
+
+
+
+        }
+    }
